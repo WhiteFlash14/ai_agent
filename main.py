7e98d28b7bd777b48bee67af55702d3f0c30ff6f
nish <nishchal1405@gmail.com>
2025-06-09 20:19:14 +0530

    Response feature

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    args = sys.argv[1:]
+
+    if not args:
+       print("AI Code Assistant")
+       print('\nUsage: python main.py "your prompt here"')
+       print('Example: python main.py "How do I build a calculator app?"')
+       sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=user_prompt)]
+        ),
+    ]
+    generate_response(client, messages)
+
+
+def generate_response(client, messages):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+    )
+
+    
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()

d60deafb0b7b51b73e5aade4267b3f25a5b5f720
nish <nishchal1405@gmail.com>
2025-06-09 20:46:50 +0530

    Modified response code and added calculator files to test

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-
+    
     args = sys.argv[1:]
+
+    user_prompt = " ".join(args)
+
+    for i,arg in enumerate(args):
+        if arg == "--verbose":
+            verbosity = True
+            args.pop(i)
+            print(f"User prompt: {user_prompt}\n")
+            break
 
     if not args:
        print("AI Code Assistant")
-       print('\nUsage: python main.py "your prompt here"')
+       print('\nUsage: python main.py "your prompt here"[ --verbose]')
        print('Example: python main.py "How do I build a calculator app?"')
        sys.exit(1)
 
-    user_prompt = " ".join(args)
+    
 
     messages = [
         types.Content(
@@ -26,16 +35,18 @@ def main():
             parts=[types.Part(text=user_prompt)]
         ),
     ]
-    generate_response(client, messages)
+    generate_response(client, messages,verbosity)
 
 
-def generate_response(client, messages):
+def generate_response(client, messages, verbosity):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
     )
+    if verbosity == True:
+         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    
     print("Response:")
     print(response.text)
 

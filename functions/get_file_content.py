78f54858310e9b019b27bd6bd829e9147566d7f8
nish <nishchal1405@gmail.com>
2025-06-21 01:13:07 +0530

    added run_python feature

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,9 +4,9 @@ def get_file_content(working_directory,file_path):
     base_path = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(base_path,file_path))
     if not abs_file_path.startswith(base_path):
-        f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(file_path):
-        f'Error: File not found or is not a regular file: "{file_path}"'
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     MAX_CHAR = 10000
 
     try:
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
-
+from functions.run_python import run_python_file
 def test():
     # result = get_files_info("calculator", ".")
     # print("Result for current directory:")
@@ -26,24 +26,31 @@ def test():
     # print("Result for current directory:")
     # print(result)
     # print("")
-    result = get_file_content("calculator", "main.py")
-    print("Result for current directory:")
-    print(result)
-    print("")
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for pkg directory:")
-    print(result)
-    print("")
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for /bin directory:")
-    print(result)
-    print("")
+    # result = get_file_content("calculator", "main.py")
+    # print("Result for current directory:")
+    # print(result)
+    # print("")
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print("Result for pkg directory:")
+    # print(result)
+    # print("")
+    # result = get_file_content("calculator", "/bin/cat")
+    # print("Result for /bin directory:")
+    # print(result)
+    # print("")
 
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # print(result)
+    # result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    # print(result)
+    # result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    # print(result)
+
+    run_python_file("calculator", "main.py")
+    run_python_file("calculator", "tests.py")
+    result = run_python_file("calculator", "../main.py") 
     print(result)
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(result)
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "nonexistent.py")
     print(result)
 
 if __name__ == "__main__":
